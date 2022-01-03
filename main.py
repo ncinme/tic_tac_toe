e945b1282ac225776f191f3c9e28bcd5d4a9dff5
ncinme <ncinme2020@gmail.com>
2022-01-03 08:48:16 +0530

    random bot

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
 # This program is to build a text-based version of the Tic Tac Toe game - playable in the command line
-# This is a Two players version
+# This is a Random Bot version
 # Allows the user to play again
 # Keeps the score
+
+from random import randint
 
 # Logging into a file on the server
 import logging
 LOG_FORMAT = "%(levelname)s %(asctime)s -- %(message)s"
 logging.basicConfig(filename='ttt.log', level=logging.INFO, format=LOG_FORMAT, filemode='a')
 
-# We can either hardcode or calculate the winning combination
-# winning_combination = [[(0, 0), (0, 1), (0, 2)], [(1, 0), (1, 1), (1, 2)], [(2, 0), (2, 1), (2, 2)],
-#                        [(0, 0), (1, 0), (2, 0)], [(0, 1), (1, 1), (2, 1)], [(0, 2), (1, 2), (2, 2)],
-#                        [(0, 0), (1, 1), (2, 2)], [(0, 2), (1, 1), (2, 0)]]
-
+# Calculate the winning combinations
 winning_rows = [[(i, j) for j in range(0, 3)] for i in range(0, 3)]
 winning_cols = [[(j, i) for j in range(0, 3)] for i in range(0, 3)]
 winning_diagonal_1 = [[item[winning_rows.index(item)] for item in winning_rows]]
 winning_diagonal_2 = [[item[len(item) - winning_rows.index(item) - 1] for item in winning_rows]]
 winning_combination = winning_rows + winning_cols + winning_diagonal_1 + winning_diagonal_2
 
-# Initialize variables
-count = 1
-player1_selection = []
-player2_selection = []
-player1_score = 0
-player2_score = 0
-row1 = ['___', '___', '___']
-row2 = ['___', '___', '___']
-row3 = ['   ', '   ', '   ']
-block = [row1, row2, row3]
 
 def print_ttt():
     print(*row1, sep="|")
@@ -47,12 +35,18 @@ def play_again():
 
 def play_game(player):
     try:
-        row = int(input('Choose a row 1, 2 or 3: ')) - 1
-        col = int(input('Choose a column 1, 2 or 3: ')) - 1
+        if player == 1:
+            row = int(input('Choose a row 1, 2 or 3: ')) - 1
+            col = int(input('Choose a column 1, 2 or 3: ')) - 1
+        else:
+            row = randint(0, 2)
+            col = randint(0, 2)
+
         player_selection = [(row, col)]
 
         if set(player_selection).issubset(set(player1_selection)) or set(player_selection).issubset(set(player2_selection)):
-            print(f"This option is already taken. Player {player}, please choose again!")
+            if player == 1:
+                print(f"This option is already taken. Player {player}, please choose again!")
             play_game(player)
         else:
             if player == 1:
@@ -72,14 +66,23 @@ def play_game(player):
         logging.exception(err)
         play_game(player)
 
+# Initialize variables
+count = 1
+player1_selection = []
+player2_selection = []
+player1_score = 0
+player2_score = 0
+row1 = ['___', '___', '___']
+row2 = ['___', '___', '___']
+row3 = ['   ', '   ', '   ']
+block = [row1, row2, row3]
+print_ttt()
 
 while count < 10:
-    print_ttt()
-    player = 1
     game_end = False
+    player = 1
     if count % 2 == 0:
         player = 2
-    print(f'Player {player} turn:')
     try:
         play_game(player)
         for item in winning_combination:
@@ -89,11 +92,9 @@ while count < 10:
                     player1_score += 1
                 else:
                     player2_score += 1
-
                 print(f"Game Over!! Player {player} is the winner!!")
                 print(f"Score: Player1 = {player1_score} ; Player2 = {player2_score}")
                 game_end = True
-
             elif count == 9:
                 print(f"It's a Draw!!")
                 print(f"Score: Player1 = {player1_score} ; Player2 = {player2_score}")
@@ -114,6 +115,10 @@ while count < 10:
             else:
                 print("Thank you. Hope you enjoined the game!!")
                 break
+        else:
+            if player == 2:
+                print_ttt()
+
         count += 1
 
     except Exception as err:
